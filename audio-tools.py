2831faa4d45cc439e7327c8d9f180cfd3248d750
frdcmp <fdc1992@gmail.com>
2023-09-22 15:24:06 +0700

    Main Update

--- audio-tools.py
+++ audio-tools.py
@@ -4,8 +4,5 @@ import os
 
 # Streamlit app title
 st.title("Audio Tools")
-st.markdown('## Audio tools for big audio guys')
+st.markdown('## Audio tools for audio guys')
 st.markdown('[GitHub Repository](https://github.com/frdcmp/silence-manipulator)')
-
-st.markdown('## How to Use')
-st.markdown('Ask Francesco')
